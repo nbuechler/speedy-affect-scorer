1687da7a6bed66db2918b631835c7921b24146d5
nbuechler <natebuechler@gmail.com>
2016-06-11 22:11:14 -0400

    actually using nltk to remove stop words

--- app/helpers/controllers.py
+++ app/helpers/controllers.py
@@ -1,3 +1,15 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import wordpunct_tokenize
+
 def default():
     print 'Here'
     return 'Thanks controller, hello helpers!'
+
+def length_no_stop(doc, lang):
+
+    stop_words = set(stopwords.words(lang))
+    stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}']) # remove it if you need punctuation
+
+    list_of_words = [i for i in wordpunct_tokenize(doc) if i.lower() not in stop_words]
+
+    return len(list_of_words)
